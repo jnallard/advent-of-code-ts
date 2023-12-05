6378b93a171da4b068de4640a962645f3cb5944c
Joshua Allard <joallard@cimpress.com>
2023-12-04 21:44:47 -0500

    2023 - Days 1 & 2

--- helpers.ts
+++ helpers.ts
@@ -3,3 +3,20 @@ export const sum = (nums: number[]) => nums.reduce((sum, newValue) => sum + newV
 export function intersect<T>(...sets: Set<T>[]) { return sets.reduce((a, b) => new Set([...a].filter(x => b.has(x)))) }
 
 export const isBetween = (value: number, min: number, max: number, inclusive: boolean) => inclusive ? value >= min && value <= max : value > min && value < max;
+
+export const execPart = (func: () => string | number, partName: string) => {
+  console.log(`Starting ${partName}...`);
+  const startTime = new Date().getTime();
+  const result = func();
+  const endTime = new Date().getTime();
+  console.log(`${partName}: ${result} \t\t (${endTime - startTime}ms)`)
+  console.log(`==================`);
+}
+
+export const execPart1 = (func: () => string | number) => {
+  execPart(func, 'Part 1');
+}
+
+export const execPart2 = (func: () => string | number) => {
+  execPart(func, 'Part 2');
+}
