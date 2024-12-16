36807a9e5b8bf3da7b0faedcfc68d03eaa90d241
Joshua Allard <jnallard@users.noreply.github.com>
2024-12-16 00:39:02 -0500

    2024 Day 15 Part 2

--- src/2024/day15.ts
+++ src/2024/day15.ts
@@ -3,14 +3,18 @@
 */
 
 
-import { ArrowDirection, ArrowDirections, Coordinate, CoordinateTransponse, E, Grid, N, S, W } from "../grid-helpers";
+import { ArrowDirection, ArrowDirections, Coordinate, E, Grid, W } from "../grid-helpers";
 import { execExamplePart1, execExamplePart2, execPart1, execPart2, sum } from "../helpers";
-import { INPUT_WAREHOUSE, INPUT_MOVES, SMALL_SAMPLE_INPUT_WAREHOUSE, SMALL_SAMPLE_INPUT_MOVES, BIG_SAMPLE_INPUT_WAREHOUSE, BIG_SAMPLE_INPUT_MOVES } from "./input/input-day15";
+import { BIG_SAMPLE_INPUT_MOVES, BIG_SAMPLE_INPUT_WAREHOUSE, INPUT_MOVES, INPUT_WAREHOUSE, SMALL_SAMPLE_INPUT_MOVES, SMALL_SAMPLE_INPUT_WAREHOUSE } from "./input/input-day15";
 
 const WALL = '#';
 const ROBOT = '@';
 const BOX = 'O';
+const LEFT_BOX = '[';
+const RIGHT_BOX = ']';
 const OPEN_SPACE = '.';
+
+const WallException = 'wall';
 
 function sharedSetup(widen: boolean, warehouseInput: string = INPUT_WAREHOUSE, movesInput: string = INPUT_MOVES) {
     if (widen) {
@@ -22,41 +26,77 @@ function sharedSetup(widen: boolean, warehouseInput: string = INPUT_WAREHOUSE, m
     }
     const warehouse = new Grid(warehouseInput.split('\n').filter(l => !!l));
     const moves = movesInput.split('\n').filter(l => !!l).join('').split('') as ArrowDirection[];
-    // console.log({warehouse, moves});
     return {warehouse, moves};
+}
+
+function getAllBoxesVertically(warehouse: Grid, arrow: ArrowDirection, pose: Coordinate, allBoxes: Record<string, Coordinate> = {}) {
+    if (allBoxes[pose.id] !== undefined) {
+        return Object.values(allBoxes);
+    }
+    const direction = ArrowDirections[arrow];
+    const boxHalves = warehouse.getNeighbors(pose, {directions: [direction], recursive: true, valuesToMatch: [LEFT_BOX, RIGHT_BOX]});
+    const lastBox = boxHalves[boxHalves.length - 1];
+    const nextPose = warehouse.getCoordWithTranspose(lastBox, direction);
+    if (nextPose.value === WALL) {
+        throw WallException;
+    }
+    const otherHalves = boxHalves.map(b => warehouse.getCoordWithTranspose(b, b.value === ']' ? W : E));
+    boxHalves.forEach(b => allBoxes[b.id] = b);
+    otherHalves.forEach(b => getAllBoxesVertically(warehouse, arrow, b, allBoxes));
+    return Object.values(allBoxes);
 }
 
 function makeMove(warehouse: Grid, arrow: ArrowDirection, robotPose: Coordinate) {
     const direction = ArrowDirections[arrow];
     const nextMove = warehouse.getCoordWithTranspose(robotPose, direction);
     if (nextMove.value === WALL) {
-        // console.log('wall');
         return robotPose;
     }
     if (nextMove.value === OPEN_SPACE) {
-        // console.log('moves');
         nextMove.value = ROBOT;
         robotPose.value = OPEN_SPACE;
         return nextMove;
     }
     if (nextMove.value === BOX) {
         const boxes = warehouse.getNeighbors(robotPose, {directions: [direction], recursive: true, valuesToMatch: [BOX]});
-        // console.log(boxes);
         const lastBox = boxes[boxes.length - 1];
         const afterLastBox = warehouse.getCoordWithTranspose(lastBox, direction);
         if (afterLastBox.value === WALL) {
-            // console.log('wall after boxes');
             return robotPose;
         }
-        // console.log('moved boxes');
         afterLastBox.value = BOX;
         robotPose.value = OPEN_SPACE;
         nextMove.value = ROBOT;
         return nextMove;
     }
-    // console.log(direction, robotPose);
-    // warehouse.print();
-    throw 'what do i do'
+    if (arrow === '>' || arrow === '<') {
+        const boxes = warehouse.getNeighbors(robotPose, {directions: [direction], recursive: true, valuesToMatch: [LEFT_BOX, RIGHT_BOX]});
+        const lastBox = boxes[boxes.length - 1];
+        const afterLastBox = warehouse.getCoordWithTranspose(lastBox, direction);
+        if (afterLastBox.value === WALL) {
+            return robotPose;
+        }
+        boxes.forEach(b => b.value = b.value === LEFT_BOX ? RIGHT_BOX : LEFT_BOX);
+        afterLastBox.value = arrow === '>' ? RIGHT_BOX : LEFT_BOX;
+        robotPose.value = OPEN_SPACE;
+        nextMove.value = ROBOT;
+        return nextMove;
+    }
+    try {
+        const allBoxesVertically = getAllBoxesVertically(warehouse, arrow, robotPose);
+        const clonedCoords = allBoxesVertically.map(c => ({...c} as Coordinate));
+        allBoxesVertically.forEach(b => b.value = OPEN_SPACE);
+        clonedCoords.forEach(b => warehouse.getCoordWithTranspose(b, direction).value = b.value);
+        robotPose.value = OPEN_SPACE;
+        nextMove.value = ROBOT;
+        return nextMove;
+    } catch (e) {
+        if (e === WallException) {
+            // Hit a wall at some point, so can't move anything;
+            return robotPose;
+        }
+        throw e;
+    }
 }
 
 function calculateGpsScore(box: Coordinate) {
@@ -66,12 +106,10 @@ function calculateGpsScore(box: Coordinate) {
 function makeAllMoves(warehouse: Grid, moves: ArrowDirection[]) {
     let robotPose = warehouse.coordsByValue[ROBOT][0];
     for (let move of moves) {
-        // console.log(move, robotPose);
         robotPose = makeMove(warehouse, move, robotPose);
-        // warehouse.print();
     }
     warehouse.updateCoordMap();
-    const boxes = warehouse.coordsByValue[BOX];
+    const boxes = [...warehouse.coordsByValue[BOX] ?? [], ...warehouse.coordsByValue[LEFT_BOX] ?? []];
     return sum(boxes.map(b => calculateGpsScore(b)));
 }
 
@@ -85,18 +123,23 @@ execExamplePart1(() => {
     return makeAllMoves(warehouse, moves);
 }, 'big example')
 
-// execExamplePart2(() => {
-//         const {warehouse, moves} = sharedSetup(true, SMALL_SAMPLE_INPUT_WAREHOUSE, SMALL_SAMPLE_INPUT_MOVES);
-//         return makeAllMoves(warehouse, moves);
-// }, 'small example')
+execExamplePart2(() => {
+        const {warehouse, moves} = sharedSetup(true, SMALL_SAMPLE_INPUT_WAREHOUSE, SMALL_SAMPLE_INPUT_MOVES);
+        return makeAllMoves(warehouse, moves);
+}, 'small example')
+
+execExamplePart2(() => {
+    const {warehouse, moves} = sharedSetup(true, BIG_SAMPLE_INPUT_WAREHOUSE, BIG_SAMPLE_INPUT_MOVES);
+    return makeAllMoves(warehouse, moves);
+}, 'big example')
 
 execPart1(() => {
     const {warehouse, moves} = sharedSetup(false);
     return makeAllMoves(warehouse, moves);
 })
 
-// execPart2(() => {
-//     const {lines} = sharedSetup();
-//     return 'TBD';
-// })
+execPart2(() => {
+        const {warehouse, moves} = sharedSetup(true);
+        return makeAllMoves(warehouse, moves);
+})
 
