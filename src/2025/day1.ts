2c9de8e63e3240a03e9f5355cbb3e6ace1478640
Joshua Allard <joallard@cimpress.com>
2025-12-09 16:34:53 -0500

    Start 2025

---
+++ src/2025/day1.ts
@@ -0,0 +1,33 @@
+/*
+
+*/
+
+
+import { execExamplePart1, execExamplePart2, execPart1, execPart2 } from "../helpers";
+import { INPUT, SAMPLE_INPUT } from "./input/input-day1";
+
+function sharedSetup(input: string = INPUT) {
+    const lines = input.split('\n').filter(l => !!l);
+    return {lines};
+}
+
+execExamplePart1(() => {
+    const {lines} = sharedSetup(SAMPLE_INPUT);
+    return 'TBD';
+})
+
+execExamplePart2(() => {
+    const {lines} = sharedSetup(SAMPLE_INPUT);
+    return 'TBD';
+})
+
+execPart1(() => {
+    const {lines} = sharedSetup();
+    return 'TBD';
+})
+
+execPart2(() => {
+    const {lines} = sharedSetup();
+    return 'TBD';
+})
+
---
+++ src/2025/input/input-day1.ts
@@ -0,0 +1,7 @@
+export const SAMPLE_INPUT = `
+
+`
+
+export const INPUT = `
+
+`
